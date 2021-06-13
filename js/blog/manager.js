ab3b6b8427cf0d612fcda989a73665ea50f6d6f8
alanmatzumiya <alan.matzumiya@outlook.es>
2021-06-12 18:18:57 -0700

    auto

--- js/blog/manager.js
+++ js/blog/manager.js
@@ -1,8 +1,44 @@
 var API = "https://circuitalminds-storage-app.herokuapp.com";
-var USER_ONLINE = "Alan Matzumiya";
-window.onload = function() {
-  document.getElementById("greeting").innerHTML = "Welcome";
-  document.getElementById("user-online").innerHTML = USER_ONLINE;
+
+var users = [
+	{username: "alanmatzumiya", password: "perrito"},
+	{username: "seleneguzman", password: "gatito"}
+];
+
+function get_user_data () {
+	return {
+		username: $('#username')[0], 
+		password: $('#password')[0]
+	};
+};
+
+function Login () {
+	data = get_user_data();
+	user = data.username.value;
+	pwd = data.password.value;
+	id = users.map( q => q.username == user ).indexOf(true)
+	if ( id != -1 ) {
+		user_data = users[id];
+		if ( user_data.password == pwd ) {
+			$('#greeting')[0].innerHTML = "Welcome";					
+			$('#user-online')[0].innerHTML = user_data.username;	
+			$('#login-message')[0].innerHTML = "";			
+			data.username.value = '';
+			data.password.value = '';
+		} else {			
+			$('#greeting')[0].innerHTML = "";
+			$('#user-online')[0].innerHTML = "";
+			$('#login-message')[0].innerHTML = "Login data incorrect";									
+			data.username.value = '';
+			data.password.value = '';
+		};
+	} else {
+		$('#greeting')[0].innerHTML = "";
+		$('#user-online')[0].innerHTML = "";
+		$('#login-message')[0].innerHTML = "Login data incorrect";								
+		data.username.value = '';
+		data.password.value = '';
+	}
 };
 
 function ManagerRequest ( Obj ) {
