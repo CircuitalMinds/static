d98df854e34fed02086abe746ca1aea3cdaf66b3
alanmatzumiya <alan.matzumiya@outlook.es>
2022-01-25 07:58:29 -0700

    autocommit

--- js/api.js
+++ js/api.js
@@ -29,44 +29,26 @@ function Dict ( data ) {
     return Obj;
 };
 
-var Api = new Object();
-Api.responses = {
-    data: [],
-    scheme: function ( url, datatype="json" ) {
-        var obj = {request: url, response: ( datatype == "json" ) ? {} : "" };
-        return obj;
-    }
+let urlObj = new Object();
+urlObj.Host = "https://circuitalminds.github.io";
+urlObj.get = function ( Paths ) {
+    Url = [this.Host];
+    Paths.map( x => Url.push(x) );
+    return Url.join("/");
 };
 
-Api.call = function ( method, query, params={} ) {
-    isMethod = ( method != undefined ) ? this.Data.methods.indexOf(method) : false;
-    isQuery = ( query != undefined ) ? this.Data.books.indexOf(query) : false;
-    isQuery = ( query != undefined ) ? this.Data.resources.indexOf(query) : false;
-    if ( isMethod || isQuery ) {
-        url = [this.Data.url, method, query].join("/");
-        if ( Object.keys(params).length > 0 ) {
-            url += "?" + Object.keys(params).map( x => x + "=" + params[x] ).join("&");
-        };
-        obj = this.responses.scheme(url);
-        $.get(url).done(function (data) {
-            console.log(data);
-            obj.response = data;
-            this.responses.data.push(obj);
-        });
-    } else {
-        return {response: "request invalid"};
-    };
+let requestObj = new Object();
+requestObj.get = function ( urlData, handlerData ) {
+    $.getJSON(
+        urlData, function ( data ) {
+            setTimeout( function () {handlerData( data )}, 200 );
+        }
+    );
 };
-
-Api.getGeolocation = function() {
-    if ( navigator.geolocation ) {
-        navigator.geolocation.watchPosition(
-            function ( position ) {
-                ["latitude", "longitude"].map(
-                    console.log(position.coords[k])
-                );
-            }
-        );
-    };
-    console.log(this.Data.geolocation);
+requestObj.post = function ( urlData, Data, handlerData ) {
+    $.post(
+        urlData, Data, function ( data ) {
+            setTimeout( function () {handlerData( data )}, 200 );
+        }
+    );
 };
